52ee3c13214f59b9cff6d9d261ee89694bb11352
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-08-12 01:05:34 +0530

    Revert "first"
    This reverts commit f76b34442ae70b8d2ee610408b4f057041693ea3.

--- src/containers/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Blog} from '../containers/blog/Blog.jsx';
-export {default as Features} from '../containers/features/Features.jsx';
-export {default as Footer} from '../containers/footer/Footer.jsx';
-export {default as Header} from '../containers/header/Header.jsx';
-export {default as Possibility} from '../containers/possibility/Possibility.jsx';
-export {default as WhatGPT3} from '../containers/whatGPT3/WhatGPT3.jsx';
